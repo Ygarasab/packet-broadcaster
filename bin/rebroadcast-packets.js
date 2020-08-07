5af5eb6b57b738d5ac777165f69f0d1e04f77600
Eduardo Cardoso <ygarasab@gmail.com>
2020-08-07 13:36:32 -0300

    feat : added verbose

--- bin/rebroadcast-packets.js
+++ bin/rebroadcast-packets.js
@@ -9,10 +9,11 @@ const options = yargs
     .usage("Usage : [-d <device>] [-p <port>]")
     .option("d", {alias : "device", describe : "device to listen to", type : "string", default : ''})
     .option("p", {alias : "port", describe : "port for sockets", type : 'number', default : 7000})
+    .option("v", {alias : "verbose", boolean : true})
     .argv
 
 console.log("Runnig Packet Broadcaster, from Ygarasab")
 console.log("Listenning to device : ", options.d || 'all')
 console.log("Emmitting through port :", options.d)
 
-rebroadcaster.launchRebroadcaster(optionsp, options.d)
+rebroadcaster.launchRebroadcaster(optionsp, options.d, options.v)
--- rebroadcaster/index.js
+++ rebroadcaster/index.js
@@ -5,15 +5,25 @@ const http = require("http")
 const server = http.createServer()
 const io = sockets.getSocket(server)
 
+const c = 0;
+
 module.exports = {
 
     /**
      * @param {Number} port
      * @param {String} device
      */
-    launchRebroadcaster : (port, device) => {
+    launchRebroadcaster : (port, device, verbose) => {
 
-        receptor.listen(device, packet => io.emit('packet', packet))
+        
+
+        receptor.listen(device, packet =>{
+            io.emit('packet', packet)
+            if(verbose){
+                c++
+                console.log("\rPacotes enviados : " + c)
+            }
+        })
         server.listen(port)
 
     }
