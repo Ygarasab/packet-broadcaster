a21b9429c778c9bfa0c1a78404595706df252573
Teccon <teccon@inesc>
2020-08-08 17:02:25 -0300

    feat : a working mess

--- rebroadcaster/index.js
+++ rebroadcaster/index.js
@@ -4,7 +4,32 @@ const http = require("http")
 const server = http.createServer()
 
 const WebSocket = require('ws')
-const wss = new WebSocket.Server({ port: 7000 });
+const wss = new WebSocket.Server({ port: 7000, perMessageDeflate: {
+    zlibDeflateOptions: {
+      // See zlib defaults.
+      chunkSize: 1024,
+      memLevel: 7,
+      level: 3
+    },
+    zlibInflateOptions: {
+      chunkSize: 10 * 1024
+    },
+    // Other options settable:
+    clientNoContextTakeover: true, // Defaults to negotiated value.
+    serverNoContextTakeover: true, // Defaults to negotiated value.
+    serverMaxWindowBits: 10, // Defaults to negotiated value.
+    // Below options specified as default values.
+    concurrencyLimit: 10, // Limits zlib concurrency for perf.
+    threshold: 1024 // Size (in bytes) below which messages
+    // should not be compressed.
+  }});
+
+var ps = Buffer.from([])
+
+wss.on('connection', ws => {
+    console.log('hi')
+
+})
 
 /**
  * @param {Number} port
@@ -12,18 +37,25 @@ const wss = new WebSocket.Server({ port: 7000 });
  * @param {String} binder
  * @param {Boolean} verbose
  */
-const launchRebroadcaster = (port, live, binder, verbose) => {
+const launchRebroadcaster = (port, binder, verbose) => {
 
-    setInterval( crunchPackets, 1000)
-
-    const listener = live ? receptor.listenDevice : receptor.listenPcap
-    listener(binder, packet => wss.clients.forEach(
+setInterval(() => {
+ 
+    console.log(new Date())
+    wss.clients.forEach(
         client => {
-            client.send(packet)
+            
+            client.send(ps)
         }
-    ))
-    server.listen(port)
+    )
 
+ps = Buffer.from([])
+
+}, 1000)
+
+    receptor.listenDevice(binder, packet => {
+    ps = Buffer.concat([ps, packet.subarray(0, 1424)])
+})
 }
 
 
