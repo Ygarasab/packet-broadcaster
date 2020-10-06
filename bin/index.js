879ded6041f48dc6b45a36d34f79897aaf22eded
Eduardo Cardoso <ygarasab@gmail.com>
2020-10-06 15:45:02 -0300

    feat : setting device to teccon's default

--- bin/index.js
+++ bin/index.js
@@ -7,7 +7,7 @@ const yargs = require('yargs')
 const options = yargs
 
     .usage("Usage : [-d <device>] [-p <port>] ...")
-    .option("d", {alias : "device", describe : "device to listen to", type : "string", default : ''})
+    .option("d", {alias : "device", describe : "device to listen to", type : "string", default : 'enp0s31f6'})
     .option("p", {alias : "port", describe : "port for sockets", type : 'number', default : 7000})
     .option("s", {alias : "packet-size", describe : "length of each individual packet", type : 'number', default : 1424})
     .option("c", {alias : "packet-count", describe : "Number of packets per emitted Buffer", type : 'number', default : 131})
