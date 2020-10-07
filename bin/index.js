d5f03f313226ca77f7cb959256bffb2a7a1a12b5
Teccon <teccon@inesc>
2020-10-06 22:57:06 -0300

    refactor : default batch size adjustment

--- bin/index.js
+++ bin/index.js
@@ -10,7 +10,7 @@ const options = yargs
     .option("d", {alias : "device", describe : "device to listen to", type : "string", default : 'enp0s31f6'})
     .option("p", {alias : "port", describe : "port for sockets", type : 'number', default : 7000})
     .option("s", {alias : "packet-size", describe : "length of each individual packet", type : 'number', default : 1424})
-    .option("c", {alias : "packet-count", describe : "Number of packets per emitted Buffer", type : 'number', default : 131})
+    .option("c", {alias : "packet-count", describe : "Number of packets per emitted Buffer", type : 'number', default : 128})
     .argv
 
 console.log("Runnig Packet Rebroadcaster, by Ygarasab")
